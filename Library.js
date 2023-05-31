f488f8613e9811c343a57722d740631be271fe5b
COR Coding <corcoding@CORs-MacBook-12.localdomain>
2023-05-31 09:06:38 -0400

    initial commit

---
+++ Library.js
@@ -0,0 +1,28 @@
+class Library {
+    constructor() {
+        this.inventory = [];
+    }
+
+    addBook(book) {
+        this.inventory.push(book);
+    }
+
+    issueBook(book) {
+        const index = this.inventory.indexOf(book);
+        if (index !== -1) {
+            this.inventory.splice(index, 1);
+            return true;
+        }
+        return false;
+    }
+
+    acceptBook(book) {
+        this.inventory.push(book);
+    }
+
+    displayInventory() {
+        return this.inventory;
+    }
+}
+
+module.exports = Library;
